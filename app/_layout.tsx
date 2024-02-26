56f8b3a507d436224f5fdaae769aa80315c60237
David Opoku <david@cyboticx.com>
2024-02-26 03:55:59 +0000

    setup base layout, setup unistyles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,24 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "../utils/unistyles";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -22,8 +26,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    SFProDisplay: require("../assets/fonts/SFProDisplay-Regular.ttf"),
+    SFProDisplayMedium: require("../assets/fonts/SFProDisplay-Medium.ttf"),
+    SFProDisplayBold: require("../assets/fonts/SFProDisplay-Bold.ttf"),
+    SFProDisplaySemibold: require("../assets/fonts/SFProDisplay-Semibold.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -48,10 +54,11 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="index" />
+        <Stack.Screen name="settings" options={{ presentation: "modal" }} />
+        <Stack.Screen name="news/[id]" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+
+import { createStyleSheet, useStyles } from "react-native-unistyles";
+
+export default function Screen() {
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Unistyles example</Text>
+    </View>
+  );
+}
+
+const stylesheet = createStyleSheet((theme) => ({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.background,
+  },
+  text: {
+    color: theme.colors.typography,
+  },
+}));
